3aeb309936635c0c90e15244da75b71faf086ad2
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-05-24 23:32:21 +0100

    feat: add all fetch recent questions flow

--- src/domain/forum/application/use-cases/fetch-recent-questions.ts
+++ src/domain/forum/application/use-cases/fetch-recent-questions.ts
@@ -1,6 +1,7 @@
 import { right, type Either } from '@/core/either'
 import { Question } from '../../enterprise/entities/question'
-import type { QuestionsRepository } from '../repositories/questions-repository'
+import { QuestionsRepository } from '../repositories/questions-repository'
+import { Injectable } from '@nestjs/common'
 
 interface FetchRecentQuestionsUseCaseRequest {
   page: number
@@ -11,6 +12,7 @@ type FetchRecentQuestionsUseCaseResponse = Either<
   { questions: Question[] }
 >
 
+@Injectable()
 export class FetchRecentQuestionsUseCase {
   constructor(private questionsRepository: QuestionsRepository) {}
 
