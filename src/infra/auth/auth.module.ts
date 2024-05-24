4a0fb19ebf0a3da9b60c938cee266323a5b9169a
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-05-24 21:49:11 +0100

    feat: add infra layer

--- src/infra/auth/auth.module.ts
+++ src/infra/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common'
 import { PassportModule } from '@nestjs/passport'
 import { JwtModule } from '@nestjs/jwt'
 import { ConfigService } from '@nestjs/config'
-import type { Env } from '@/env'
+import type { Env } from '@/infra/env'
 import { JwtStrategy } from './jwt.strategy'
 
 @Module({
