282c067e7a7c59cfb8269f82cebcf0776b8266be
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-05-26 15:21:59 +0100

    feat: add prisma answers repository implementation

--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -63,10 +63,10 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
   }
 
   async delete(question: Question): Promise<void> {
-    const data = PrismaQuestionMapper.toPrisma(question)
-
     await this.prisma.question.delete({
-      where: { id: data.id },
+      where: {
+        id: question.id.toString(),
+      },
     })
   }
 }
