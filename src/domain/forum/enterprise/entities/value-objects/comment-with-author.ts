f8b04246d7d6106f04a8f5dab8358fa30ae8b95c
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-05-30 19:52:54 +0100

    feat: add author data on fetch question comments (domain layer)

--- src/domain/forum/enterprise/entities/value-objects/comment-with-author.ts
+++ src/domain/forum/enterprise/entities/value-objects/comment-with-author.ts
@@ -1,33 +1,30 @@
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import { ValueObject } from '@/core/entities/value-object'
 
 export interface CommentWithAuthorProps {
-  comment: {
-    id: string
-    content: string
-  }
-  author: {
-    id: string
-    name: string
-  }
+  commentId: UniqueEntityId
+  content: string
+  authorId: UniqueEntityId
+  author: string
   createdAt: Date
   updatedAt?: Date | null
 }
 
 export class CommentWithAuthor extends ValueObject<CommentWithAuthorProps> {
   get commentId() {
-    return this.props.comment.id
+    return this.props.commentId
   }
 
   get content() {
-    return this.props.comment.content
+    return this.props.content
   }
 
   get authorId() {
-    return this.props.author.id
+    return this.props.authorId
   }
 
-  get authorName() {
-    return this.props.author.name
+  get author() {
+    return this.props.author
   }
 
   get createdAt() {
